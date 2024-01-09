d68b63e4febb6cc70e0cc85a0ea6abab17972448
jiwon <jna2158@deepnoid.com>
2024-01-09 16:25:26 +0900

    feat: auth 코드 받아오기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
 function App() {
+  const CLIENT_ID = "2d8016749bbd32ae6daa139bdd487420";
+  const REDIRECT_URI = "https://ticket-interview.com/kakao/login";
+  const RESPONSE_TYPE = 'code';
+  // kakao에서 제공하는 주소 => 인가코드를 받아오는 주소
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
+
+  /** 로그인 */
+  const login = () => {
+    window.open(KAKAO_AUTH_URL, "_blank", "width=400, height=500, left=500, top=400");
+  }
+
   return (
     <div className="App">
-      <button>login</button>
+      <button onClick={() => login()}>login</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Auth } from './auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+      <Routes>
+        <Route path='/kakao/login' element={<Auth />}/>
+      </Routes>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// < BrouserRouter: History API를 사용해 현재 위치의 URL을 저장해주는 역할 >
 reportWebVitals();
+// http://localhost:8080/kakao/login?code=ZDmt3H9vfp-eZ9RcTKYWI46xSq8qGFMi34HTieCwdSP_48qD1CK83TSHCTUKKcjYAAABjOzy0_hV7imzm104lw
